017fa0863f24fd9b73f63ddb8a6f84b0105a62a1
Hari Jiang <hari.jiang@outlook.com>
2016-04-06 17:30:26 +0800

    use absolute path

--- tests/test_dockerfile.py
+++ tests/test_dockerfile.py
@@ -1,7 +1,11 @@
 # coding=utf8
 from __future__ import unicode_literals
 
+import os
+
+project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
+
 
 def test_dockerfile():
-    with open('../Dockerfile') as f:
+    with open('{}/Dockerfile'.format(project_path)) as f:
         assert 'FROM ubuntu:trusty' in f.read()
--- tests/test_readme.py
+++ tests/test_readme.py
@@ -2,8 +2,11 @@
 from __future__ import unicode_literals
 
 import codecs
+import os
+
+project_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
 
 def test_project():
-    with codecs.open('../README.md', encoding='utf-8') as f:
+    with codecs.open('{}/README.md'.format(project_path), encoding='utf-8') as f:
         assert 'slate' in f.read()
